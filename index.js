75e18705b2ced12b82eabe3d565209a60db5ccab
limingru <18813070947@163.com>
2017-06-28 11:25:35 +0800

    重新修改了文件名，正常播放，歌词滚动

---
+++ index.js
@@ -0,0 +1,33 @@
+$(function(){
+  $.get('./songs.json').then(function(response){
+    console.log(response)
+    console.log(typeof response)
+    //在github上response可能是字符串
+    let items = response;
+    items.forEach((i)=>{
+      let $li = $(`
+        <li>
+          <a href="./main.html?id=${i.id}" class="songName">
+            <h3>${i.name}</h3>
+          </a>
+          <p>歌手${i.id}+专辑${i.id}</p>
+            <a href="./main.html?id=${i.id}" class="playIcon">
+              <svg class="icon" aria-hidden="true">
+                <use xlink:href="#icon-play"></use>
+              </svg>
+            </a>
+        </li>
+      `)
+      $('#latestMusicList').append($li)
+    })
+    $('.loading').remove()
+
+
+
+
+  },function(){
+    alert('请求失败！')
+  })
+})
+
+
