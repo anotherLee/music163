4c903d04f661e9ea426fd27672890e8d65428cbb
limingru <18813070947@163.com>
2017-06-28 11:51:58 +0800

    修改了index.js中可能不支持模板字符串的问题

--- index.js
+++ index.js
@@ -5,19 +5,8 @@ $(function(){
     //在github上response可能是字符串
     let items = response;
     items.forEach((i)=>{
-      let $li = $(`
-        <li>
-          <a href="./main.html?id=${i.id}" class="songName">
-            <h3>${i.name}</h3>
-          </a>
-          <p>歌手${i.id}+专辑${i.id}</p>
-            <a href="./main.html?id=${i.id}" class="playIcon">
-              <svg class="icon" aria-hidden="true">
-                <use xlink:href="#icon-play"></use>
-              </svg>
-            </a>
-        </li>
-      `)
+      let $li = $('<li><a href="./main.html?id=${i.id}" class="songName"><h3>${i.name}</h3></a><p>歌手${i.id}+专辑${i.id}</p><a href="./main.html?id=${i.id}" class="playIcon"><svg class="icon" aria-hidden="true"><use xlink:href="#icon-play"></use></svg></a></li>');
+
       $('#latestMusicList').append($li)
     })
     $('.loading').remove()
